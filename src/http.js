7d18dd763247a162f98a81f20c9ad7d5be4c8bbc
duwen <duwen-pbj@sinosig.com>
2018-03-19 13:22:50 +0800

    update http module

--- src/http.js
+++ src/http.js
@@ -73,8 +73,7 @@
         /******/ 	__webpack_require__.p = "";
         /******/
         /******/ 	// Load entry module and return exports
-        /******/
-        return __webpack_require__(__webpack_require__.s = 9);
+        /******/ 	return __webpack_require__(__webpack_require__.s = 9);
         /******/ })
     /************************************************************************/
     /******/ ([
@@ -255,7 +254,7 @@
                                     // Network error, set the status code 0
                                     if (self.status === 0) {
                                         self.statusText = responseText;
-                                        self._call("onerror", {msg: statusMessage});
+                                        self._call("onerror", { msg: statusMessage });
                                     } else {
                                         // Parsing the response headers to array in a object,  because
                                         // there may be multiple values with the same header name
@@ -372,7 +371,7 @@
                     this.config = {
                         method: "GET",
                         baseURL: "",
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                         timeout: 0,
                         withCredentials: false
                     };
@@ -387,7 +386,6 @@
                         var promise = new Promise(function (resolve, reject) {
                             options = options || {};
                             options.headers = options.headers || {};
-                            utils.merge(options.headers, _this.config.headers);
                             utils.merge(options, _this.config);
                             var rqi = _this.interceptors.request;
                             var rpi = _this.interceptors.response;
@@ -395,160 +393,157 @@
                             url = utils.trim(url || "");
                             options.method = options.method.toUpperCase();
                             options.url = url;
-                            var rqiPromise;
                             if (rqi.handler) {
-                                rqiPromise = rqi.handler(options, Promise) || options;
+                                options = rqi.handler(options, Promise) || options;
                             }
-
                             function isPromise(p) {
                                 return p instanceof Promise;
                             }
 
-                            if (!isPromise(rqiPromise)) {
-                                rqiPromise = Promise.resolve(options);
+                            if (isPromise(options)) {
+                                options.then(function (d) {
+                                    resolve(d);
+                                }, function (err) {
+                                    reject(err);
+                                });
+                                return;
                             }
-                            rqiPromise.then(function (options) {
-                                // Normalize the request url
-                                url = utils.trim(options.url);
-                                var baseUrl = utils.trim(options.baseURL || "");
-                                if (!url && isBrowser && !baseUrl) url = location.href;
-                                if (url.indexOf("http") !== 0) {
-                                    var isAbsolute = url[0] === "/";
-                                    if (!baseUrl && isBrowser) {
-                                        var arr = location.pathname.split("/");
-                                        arr.pop();
-                                        baseUrl = location.protocol + "//" + location.host + (isAbsolute ? "" : arr.join("/"));
-                                    }
-                                    if (baseUrl[baseUrl.length - 1] !== "/") {
-                                        baseUrl += "/";
-                                    }
-                                    url = baseUrl + (isAbsolute ? url.substr(1) : url);
-                                    if (isBrowser) {
-
-                                        // Normalize the url which contains the ".." or ".", such as
-                                        // "http://xx.com/aa/bb/../../xx" to "http://xx.com/xx" .
-                                        var t = document.createElement("a");
-                                        t.href = url;
-                                        url = t.href;
-                                    }
+                            // Normalize the request url
+                            url = utils.trim(options.url);
+                            var baseUrl = utils.trim(options.baseURL || "");
+                            if (!url && isBrowser && !baseUrl) url = location.href;
+                            if (url.indexOf("http") !== 0) {
+                                var isAbsolute = url[0] === "/";
+                                if (!baseUrl && isBrowser) {
+                                    var arr = location.pathname.split("/");
+                                    arr.pop();
+                                    baseUrl = location.protocol + "//" + location.host + (isAbsolute ? "" : arr.join("/"));
                                 }
-
-                                var responseType = utils.trim(options.responseType || "");
-                                engine.withCredentials = !!options.withCredentials;
-                                var isGet = options.method === "GET";
-                                if (isGet) {
-                                    if (options.body) {
-                                        if (utils.type(options.body) !== "string") {
-                                            data = utils.formatParams(options.body);
-                                        }
-                                        url += (url.indexOf("?") === -1 ? "?" : "&") + data;
-                                    }
+                                if (baseUrl[baseUrl.length - 1] !== "/") {
+                                    baseUrl += "/";
                                 }
-                                engine.open(options.method, url);
+                                url = baseUrl + (isAbsolute ? url.substr(1) : url);
+                                if (isBrowser) {
 
-                                // try catch for ie >=9
-                                try {
-                                    engine.timeout = options.timeout || 0;
-                                    if (responseType !== "stream") {
-                                        engine.responseType = responseType;
-                                    }
-                                } catch (e) {
+                                    // Normalize the url which contains the ".." or ".", such as
+                                    // "http://xx.com/aa/bb/../../xx" to "http://xx.com/xx" .
+                                    var t = document.createElement("a");
+                                    t.href = url;
+                                    url = t.href;
                                 }
+                            }
 
-                                // If the request data is json object, transforming it  to json string,
-                                // and set request content-type to "json". In browser,  the data will
-                                // be sent as RequestBody instead of FormData
-                                if (!utils.isFormData(options.body) && ["object", "array"].indexOf(utils.type(options.body)) !== -1) {
-                                    options.headers["Content-Type"] = 'application/json;charset=utf-8';
-                                    data = JSON.stringify(options.body);
+                            var responseType = utils.trim(options.responseType || "");
+                            engine.withCredentials = !!options.withCredentials;
+                            var isGet = options.method === "GET";
+                            if (isGet) {
+                                if (options.body) {
+                                    if (utils.type(options.body) !== "string") {
+                                        data = utils.formatParams(options.body);
+                                    }
+                                    url += (url.indexOf("?") === -1 ? "?" : "&") + data;
                                 }
+                            }
+                            engine.open(options.method, url);
 
-                                for (var k in options.headers) {
-                                    if (k.toLowerCase() === "content-type" && (utils.isFormData(options.body) || !options.body || isGet)) {
-                                        // Delete the content-type, Let the browser set it
-                                        delete options.headers[k];
-                                    } else {
-                                        try {
-                                            // In browser environment, some header fields are readonly,
-                                            // write will cause the exception .
-                                            engine.setRequestHeader(k, options.headers[k]);
-                                        } catch (e) {
-                                        }
-                                    }
+                            // try catch for ie >=9
+                            try {
+                                engine.timeout = options.timeout || 0;
+                                if (responseType !== "stream") {
+                                    engine.responseType = responseType;
                                 }
+                            } catch (e) {}
 
-                                function onresult(handler, data, type) {
-                                    if (handler) {
-                                        //如果失败，添加请求信息
-                                        if (type) {
-                                            data.request = options;
-                                        }
-                                        // Call response interceptor
-                                        data = handler.call(rpi, data, Promise) || data;
+                            // If the request data is json object, transforming it  to json string,
+                            // and set request content-type to "json". In browser,  the data will
+                            // be sent as RequestBody instead of FormData
+                            if (!utils.isFormData(options.body) && ["object", "array"].indexOf(utils.type(options.body)) !== -1) {
+                                options.headers["Content-Type"] = 'application/json;charset=utf-8';
+                                data = JSON.stringify(options.body);
+                            }
+
+                            for (var k in options.headers) {
+                                if (k.toLowerCase() === "content-type" && (utils.isFormData(options.body) || !options.body || isGet)) {
+                                    // Delete the content-type, Let the browser set it
+                                    delete options.headers[k];
+                                } else {
+                                    try {
+                                        // In browser environment, some header fields are readonly,
+                                        // write will cause the exception .
+                                        engine.setRequestHeader(k, options.headers[k]);
+                                    } catch (e) {}
+                                }
+                            }
+
+                            function onresult(handler, data, type) {
+                                if (handler) {
+                                    //如果失败，添加请求信息
+                                    if (type) {
+                                        data.request = options;
                                     }
-                                    if (!isPromise(data)) {
-                                        data = Promise[type === 0 ? "resolve" : "reject"](data);
+                                    // Call response interceptor
+                                    data = handler.call(rpi, data, Promise) || data;
+                                }
+                                if (!isPromise(data)) {
+                                    data = Promise[type === 0 ? "resolve" : "reject"](data);
+                                }
+                                data.then(function (d) {
+                                    resolve(d);
+                                }).catch(function (e) {
+                                    reject(e);
+                                });
+                            }
+
+                            function onerror(e) {
+                                onresult(rpi.onerror, e, -1);
+                            }
+
+                            engine.onload = function () {
+                                if (engine.status >= 200 && engine.status < 300 || engine.status === 304) {
+
+                                    // The xhr of IE9 has not response filed
+                                    var response = engine.response || engine.responseText;
+                                    if ((engine.getResponseHeader("Content-Type") || "").indexOf("json") !== -1
+                                        // Some third engine implement may transform the response text to json object automatically,
+                                        // so we should test the type of response before transforming it
+                                        && !utils.isObject(response)) {
+                                        response = JSON.parse(response);
                                     }
-                                    data.then(function (d) {
-                                        resolve(d);
-                                    }).catch(function (e) {
-                                        reject(e);
+
+                                    var headers = {};
+                                    var items = engine.getAllResponseHeaders().split("\r\n");
+                                    items.pop();
+                                    items.forEach(function (e) {
+                                        var key = e.split(":")[0];
+                                        headers[key] = engine.getResponseHeader(key);
                                     });
-                                }
-
-                                function onerror(e) {
-                                    onresult(rpi.onerror, e, -1);
-                                }
-
-                                engine.onload = function () {
-                                    if (engine.status >= 200 && engine.status < 300 || engine.status === 304) {
-
-                                        // The xhr of IE9 has not response filed
-                                        var response = engine.response || engine.responseText;
-                                        if ((engine.getResponseHeader("Content-Type") || "").indexOf("json") !== -1
-                                            // Some third engine implement may transform the response text to json object automatically,
-                                            // so we should test the type of response before transforming it
-                                            && !utils.isObject(response)) {
-                                            response = JSON.parse(response);
-                                        }
-
-                                        var headers = {};
-                                        var items = engine.getAllResponseHeaders().split("\r\n");
-                                        items.pop();
-                                        items.forEach(function (e) {
-                                            var key = e.split(":")[0];
-                                            headers[key] = engine.getResponseHeader(key);
-                                        });
-                                        var data = {data: response, headers: headers, engine: engine, request: options};
-                                        // The _response filed of engine is set in  adapter which be called in engine-wrapper.js
-                                        utils.merge(data, engine._response);
-                                        onresult(rpi.handler, data, 0);
-                                    } else {
-                                        var err = new Error(engine.statusText);
-                                        err.status = engine.status;
-                                        onerror(err);
-                                    }
-                                };
-
-                                engine.onerror = function (e) {
-                                    var err = new Error(e.msg || "Network Error");
-                                    err.status = 0;
+                                    var data = { data: response, headers: headers, engine: engine, request: options };
+                                    // The _response filed of engine is set in  adapter which be called in engine-wrapper.js
+                                    utils.merge(data, engine._response);
+                                    onresult(rpi.handler, data, 0);
+                                } else {
+                                    var err = new Error(engine.statusText);
+                                    err.status = engine.status;
                                     onerror(err);
-                                };
+                                }
+                            };
 
-                                engine.ontimeout = function () {
-                                    // Handle timeout error
-                                    var err = new Error("timeout [ " + engine.timeout + "ms ]");
-                                    err.status = 1;
-                                    onerror(err);
-                                };
-                                engine._options = options;
-                                setTimeout(function () {
-                                    engine.send(isGet ? null : data);
-                                }, 0);
-                            }).catch(function (err) {
-                                reject(err);
-                            });
+                            engine.onerror = function (e) {
+                                var err = new Error(e.msg || "Network Error");
+                                err.status = 0;
+                                onerror(err);
+                            };
+
+                            engine.ontimeout = function () {
+                                // Handle timeout error
+                                var err = new Error("timeout [ " + engine.timeout + "ms ]");
+                                err.status = 1;
+                                onerror(err);
+                            };
+                            engine._options = options;
+                            setTimeout(function () {
+                                engine.send(isGet ? null : data);
+                            }, 0);
                         });
                         promise.engine = engine;
                         return promise;
